c3a8c96995b84614bdc24637ad0566828cfbf26f
merunga <merunga@gmail.com>
2021-03-16 16:57:15 -0500

    challenge test

---
+++ 05-challenge/solution/app.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+const projectName = 'tribute-page';
+
+const switcher = document.querySelector('button#theme-switcher');
+
+switcher.addEventListener('click', function() {
+  document.body.classList.toggle('dark-theme')
+
+  const className = document.body.className;
+  if(className === "light-theme") {
+      this.textContent = "Dark";
+  }
+  else {
+      this.textContent = "Light";
+  }
+});
