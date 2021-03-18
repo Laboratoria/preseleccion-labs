333c22cb328000d1f54948a2f3591e34a235bbab
Lupe <lupemaydana@gmail.com>
2021-03-17 22:41:38 -0400

    remove final solution

--- 05-prework-exercises/01-coin-convert/solution/coinConvert.js
+++
@@ -1,9 +0,0 @@
-const coinConvert = (dollars) => {
-  const soles = dollars * 3.25;
-  const pesosMexicanos = dollars * 18;
-  const pesosChilenos = dollars * 660;
-
-  return [soles, pesosMexicanos, pesosChilenos];
-};
-
-module.exports = coinConvert;
